e6724f504be4dddef06e6a070aa50d6fdb596a28
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2019-10-09 14:32:16 -0700

    added flag to count data size in bytes

--- bq_perftest.py
+++ bq_perftest.py
@@ -4,8 +4,10 @@ from __future__ import print_function
 
 from absl import app
 from absl import flags
+import sys
 import time
 import os
+import numpy
 
 import tensorflow as tf
 from tensorflow.python.framework import ops
@@ -23,11 +25,21 @@ flags.DEFINE_integer("batch_size", 2048, "Batch size.")
 flags.DEFINE_bool("sloppy", False, 
   "If True the implementation is allowed, for the sake of expediency, to produce"
   "elements in a non-deterministic order")
+flags.DEFINE_bool("get_size_bytes", False, 
+  "Gets the data size, can slow down the test")
+
 
 DATASET_GCP_PROJECT_ID = "bigquery-public-data"
 DATASET_ID = "samples"
 TABLE_ID = "wikipedia"
 
+def get_row_size_bytes(row):
+  size_bytes = 0
+  for key, value in row.items():
+    lst = value.numpy().tolist()
+    for elem in lst:
+      size_bytes += sys.getsizeof(elem)
+  return size_bytes
 
 def run_benchmark(_):
   num_iterations = FLAGS.num_iterations
@@ -64,20 +76,35 @@ def run_benchmark(_):
   start = time.time()
   n = 0
   mini_batch = 100
+  size_bytes = 0
   for i in range(num_iterations // mini_batch):
     local_start = time.time()
     start_n = n
+    local_size_bytes = 0
     for j in range(mini_batch):
       n += batch_size
-      itr.get_next()
+      row = itr.get_next()
+      if FLAGS.get_size_bytes:
+        local_size_bytes += get_row_size_bytes(row)
+    size_bytes += local_size_bytes
+
     local_end = time.time()
     print('Processed %d entries in %f seconds. [%f] examples/s' % (
         n - start_n, local_end - local_start,
         (mini_batch * batch_size) / (local_end - local_start)))
+    if FLAGS.get_size_bytes:
+      print('%d bytes. [%f] bytes/s' % (
+          local_size_bytes,
+          (local_size_bytes) / (local_end - local_start)))
+
   end = time.time()
   print('Processed %d entries in %f seconds. [%f] examples/s' % (
       n, end - start,
       n / (end - start)))
+  if FLAGS.get_size_bytes:
+    print('%d bytes. [%f] MB/s' % (
+        size_bytes,
+        (size_bytes) / (end - start) / 1024 / 1024))
 
 
 if __name__ == '__main__':
