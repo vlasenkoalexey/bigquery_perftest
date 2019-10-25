6b5fdac3b9c527a2e092274ab3c9a5ea8508f656
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2019-10-24 17:41:02 -0700

    added GCS perftest using same dataset as BQ

--- perftest.py
+++ perftest.py
@@ -15,6 +15,9 @@ from tensorflow.python.framework import dtypes
 from tensorflow_io.bigquery import BigQueryClient
 from tensorflow_io.bigquery import BigQueryReadSession
 
+if not (hasattr(tf, "version") and tf.version.VERSION.startswith("2.")):
+  tf.compat.v1.enable_eager_execution()
+
 FLAGS = flags.FLAGS
 flags.DEFINE_string("project_id", None,
                     "GCP project id.")
@@ -27,11 +30,22 @@ flags.DEFINE_bool("sloppy", False,
   "elements in a non-deterministic order")
 flags.DEFINE_bool("get_size_bytes", False,
   "Gets the data size, can slow down the test")
-
+flags.DEFINE_enum("data_source", None, ["BQ", "GCS"], "Data source, BQ or GCS.")
+flags.mark_flag_as_required("data_source")
 
 DATASET_GCP_PROJECT_ID = "bigquery-public-data"
 DATASET_ID = "samples"
 TABLE_ID = "wikipedia"
+GCS_DATASET_FILE_PATTERN='gs://bigquery-public-data/samples/wikipedia/samples_wikipedia_*'
+FEATURE_DESCRIPTION = {
+    'title': tf.io.FixedLenFeature([], tf.string, default_value=''),
+    'id': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+    'num_characters': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+    'language': tf.io.FixedLenFeature([], tf.string, default_value=''),
+    'timestamp': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+    'wp_namespace': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+    'contributor_username': tf.io.FixedLenFeature([], tf.string, default_value=''),
+}
 
 def get_row_size_bytes(row):
   size_bytes = 0
@@ -41,10 +55,24 @@ def get_row_size_bytes(row):
       size_bytes += sys.getsizeof(elem)
   return size_bytes
 
-def run_benchmark(_):
-  num_iterations = FLAGS.num_iterations
-  batch_size = FLAGS.batch_size
-  print('Batch size: %d, Sloppy: %s' % (batch_size, FLAGS.sloppy))
+def get_dataset_from_gcs():
+  filenames = tf.io.gfile.glob(GCS_DATASET_FILE_PATTERN)
+  dataset = tf.data.Dataset.from_tensor_slices(filenames).repeat()
+  dataset = dataset.apply(tf.data.experimental.parallel_interleave(
+      map_func=tf.data.TFRecordDataset,
+      cycle_length=FLAGS.requested_streams,
+      sloppy=FLAGS.sloppy)).batch(FLAGS.batch_size)
+  return dataset
+
+def parse_row(row):
+  if FLAGS.data_source == 'GCS':
+    row = tf.io.parse_example(row, FEATURE_DESCRIPTION)
+    return row
+  else:
+    return row
+
+def get_dataset_from_bigquery():
+  print('Batch size: %d, Sloppy: %s' % (FLAGS.batch_size, FLAGS.sloppy))
   client = BigQueryClient()
   read_session = client.read_session(
       "projects/" + FLAGS.project_id,
@@ -65,14 +93,31 @@ def run_benchmark(_):
        dtypes.string],
       requested_streams=FLAGS.requested_streams
       )
-  
+
   streams = read_session.get_streams()
   print('Requested %d streams, BigQuery returned %d streams' % (
-    len(streams), 
+    len(streams),
     FLAGS.requested_streams))
-  dataset = read_session.parallel_read_rows(sloppy=FLAGS.sloppy).batch(batch_size)
-  itr = dataset.make_one_shot_iterator()
+  dataset = read_session.parallel_read_rows(sloppy=FLAGS.sloppy).batch(FLAGS.batch_size)
+  return dataset
 
+
+def run_benchmark(_):
+  data_source = FLAGS.data_source
+  batch_size = FLAGS.batch_size
+  dataset = None
+  if data_source == 'BQ':
+    print('Reading from BigQuery')
+    dataset = get_dataset_from_bigquery()
+  elif data_source == 'GCS':
+    print('Reading from GCS')
+    dataset = get_dataset_from_gcs()
+  else:
+    print('Invalid data_source:' + data_source)
+    exit(1)
+
+  num_iterations = FLAGS.num_iterations
+  itr = tf.compat.v1.data.make_one_shot_iterator(dataset)
   start = time.time()
   n = 0
   mini_batch = 100
@@ -83,7 +128,7 @@ def run_benchmark(_):
     local_size_bytes = 0
     for j in range(mini_batch):
       n += batch_size
-      row = itr.get_next()
+      row = parse_row(itr.get_next())
       if FLAGS.get_size_bytes:
         local_size_bytes += get_row_size_bytes(row)
     size_bytes += local_size_bytes
